fb10a266df3686130623fd1c88bbbc37f6985c7e
JamesyyD <mail@jamesy.me.uk>
2017-01-05 11:08:37 +0000

    Add files via upload

---
+++ remoteTest.ps1
@@ -0,0 +1,15 @@
+﻿$computer = 'JdC'
+
+if(Test-Connection $computer -Count 1 -Quiet) {
+    $result = Invoke-Command -ComputerName $computer {1} -ErrorAction SilentlyContinue
+    if($result -eq 1) {
+        echo 'Command will run';
+    } else {
+        echo 'Can`t run command on remote PC';
+        }
+} else {
+    echo 'Can`t contact remote PC'; 
+}
+  
+        
+           
